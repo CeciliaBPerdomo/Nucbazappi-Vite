4290359e9ec256689e1b67cbe145840eafcb15db
Cecilia Perdomo <cecilia.perdomo@gmail.com>
2025-07-02 15:58:30 -0300

    Navbar

--- README.md
+++ README.md
@@ -10,3 +10,16 @@ Currently, two official plugins are available:
 ## Expanding the ESLint configuration
 
 If you are developing a production application, we recommend using TypeScript with type-aware lint rules enabled. Check out the [TS template](https://github.com/vitejs/vite/tree/main/packages/create-vite/template-react-ts) for information on how to integrate TypeScript and [`typescript-eslint`](https://typescript-eslint.io) in your project.
+
+
+## Packages extras
+- 📌 **React Icons** 
+    - https://www.npmjs.com/package/react-icons
+    - `npm install react-icons --save`
+    - https://react-icons.github.io/react-icons/
+- 📌 **Styled Components** 
+    - https://styled-components.com/
+    - `npm install styled-components`
+- 📌 **Motion**
+    - https://motion.dev/docs/react-quick-start
+    - `npm install framer-motion`
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import { HomeContainerStyled, LinkContainerStyled, LinksContainerStyled, MenuContainerStyled, NavbarContainerStyle, SpanStyled, UserContainerStyled, UserNavStyled } from './NavbarStyle'
+import { motion } from 'framer-motion'
+
+// Icons
+import { RiHome2Fill } from "react-icons/ri";
+import { FaRegUser } from "react-icons/fa";
+import { IoMenuOutline } from "react-icons/io5";
+
+
+const Navbar = () => {
+    return (
+        <NavbarContainerStyle >
+            <div>
+                <a href="#">
+                    <img src="https://res.cloudinary.com/dcatzxqqf/image/upload/v1658797659/coding/NucbaZappi/Assets/nucba-zappi-icon_oe3ark_xmvab5.png"
+                        alt="Logo" />
+                </a>
+            </div>
+            <LinksContainerStyled>
+                <HomeContainerStyled>
+                    <motion.div whileTap={{ scale: 0.97 }}>
+                        <a href="/">
+                            <LinkContainerStyled home>
+                                <RiHome2Fill />
+                            </LinkContainerStyled> Home
+                        </a>
+                    </motion.div>
+                </HomeContainerStyled>
+
+                <UserNavStyled>
+                    <UserContainerStyled>
+                        <SpanStyled>
+                            Iniciar sesión
+                        </SpanStyled>
+                        {/* Icono de usuario */}
+                        <FaRegUser />
+                    </UserContainerStyled>
+                </UserNavStyled>
+
+                <motion.div whileTap={{ scale: 0.97 }}>
+                    <MenuContainerStyled>
+                        <IoMenuOutline />
+                    </MenuContainerStyled>
+                    </motion.div>
+            </LinksContainerStyled>
+        </NavbarContainerStyle>
+    )
+}
+
+export default Navbar
