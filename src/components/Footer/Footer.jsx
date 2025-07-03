75599962cb9813e358e18b2f4b4ec6c269fb8650
Cecilia Perdomo <cecilia.perdomo@gmail.com>
2025-07-03 18:27:58 -0300

    Footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import { FooterContainerStyled, LinksContainerStyled } from './FooterStyled'
 
 const Footer = () => {
-  return (
-    <div>Footer</div>
-  )
+    return (
+        <FooterContainerStyled>
+            <LinksContainerStyled>
+                <a href='/' style={{ paddingLeft: '2rem' }}>Términos de Uso</a>
+                <a href='/' style={{ paddingLeft: '2rem' }}>Trabajá con Nosotros</a>
+                <a href='/' style={{ paddingLeft: '2rem' }}>Soporte</a>
+            </LinksContainerStyled>
+
+            <p>
+                Hecho con ❤️ en <span> NUCBAZAPPI</span>
+            </p>
+
+        </FooterContainerStyled>
+    )
 }
 
 export default Footer
