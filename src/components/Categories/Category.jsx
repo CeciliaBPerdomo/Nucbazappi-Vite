480171d81c056e990e4778749711fb9d23364f45
Cecilia Perdomo <cecilia.perdomo@gmail.com>
2025-07-03 17:38:02 -0300

    Categories

--- src/components/Categories/Category.jsx
+++ src/components/Categories/Category.jsx
@@ -3,14 +3,8 @@ import { BorderDecoration, CardCategoria } from './CategoriesStyled'
 
 const Category = ({img, title, category}) => {
     return (
-        <CardCategoria
-            whileTap={{ scale: 1.1 }}
-        >
-            <img
-                src={img}
-                alt={category}
-            />
-
+        <CardCategoria whileTap={{ scale: 1.1 }} >
+            <img src={img} alt={category}/>
             <h2>{title}</h2>
             <BorderDecoration />
         </CardCategoria>
